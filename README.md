3914775e548cdd4c49b7a2dc389c6bb1ae547082
wooseok <80329046+Low1ife@users.noreply.github.com>
2021-10-26 15:52:53 +0900

    master

---
+++ README.md
@@ -0,0 +1,12 @@
+#React Project 
+
+## 출력하기(index.js)
+<pre>
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const name = "WooSeok";
+const hello = &lt;h1>hello {name}&lt;/h1>;
+
+ReactDom.render(hello, document.getElementById("root"));
+</pre>
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// let clock = document.getElementById("clock");
+
+// setInterval(() => {
+//   clock.innerHTML = new Date().toLocaleTimeString();
+// });
+
+function clock(){
+  const element = (
+    <div>
+      <h1>
+        Hello, Wooseok
+      </h1>
+      <h2>지금은 {new Date().toLocaleTimeString()}입니다.</h2>
+    </div>
+  )
+  ReactDom.render(element, document.getElementById("root"));
+}
+
+setInterval(clock , 1000);
+
